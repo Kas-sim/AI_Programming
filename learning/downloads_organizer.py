aa842a9f52caf5ae78d1c9ba706514a0f4a30614
Kas-sim <amkassim444@gmail.com>
2026-01-22 01:13:10 +0500

    detected file types and counter

--- learning/downloads_organizer.py
+++ learning/downloads_organizer.py
@@ -1,8 +1,25 @@
+import os
 import pathlib as path
 
-folder = "Downloads"
+folder = "D:\\Downloads"
+entries = os.listdir(folder)
 
-for file in folder:
-    if file == ".pdf":
-        create("pdf")
-        move file to folder/pdf
+print(f"{folder} contains {len(entries)} files!\n")
+
+pdf_count = 0
+doc_count = 0
+csv_count = 0
+other_types = 0
+
+for file in entries:
+    extention = path.Path(file).suffix
+    if extention == ".pdf":
+        pdf_count += 1
+    elif extention == ".docx":
+        doc_count += 1
+    elif extention == ".csv":
+        csv_count += 1
+    else:
+        other_types += 1
+        
+print(f"Total files: {len(entries)} \nPdfs: {pdf_count} \nWord files: {doc_count} \nCSV files: {csv_count}\n Other files: {other_types}")
