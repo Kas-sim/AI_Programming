68a08e3ea252a6e8ec92a57b24936738896c5633
Kas-sim <amkassim444@gmail.com>
2026-01-22 15:21:08 +0500

    working like real document analyzer

--- learning/downloads_organizer.py
+++ learning/downloads_organizer.py
@@ -8,7 +8,6 @@ print(f"{folder} contains {len(entries)} files!\n")
 
 pdf_count = 0
 office_files = 0
-csv_count = 0
 text_count = 0
 images_count = 0
 folder_count = 0
@@ -34,22 +33,29 @@ for entry in entries:
             shutil.move(entry, Office)
             print(f"Moved {entry} -> {Office}")
 
-        elif ext == ".csv":
-            csv_count += 1
+        elif ext in (".csv", ".json", ".txt", ".md"):
+            text_count += 1
+
+            text = Path("D:/Downloads/Text files")
+            text.mkdir(parents=True, exist_ok=True)
+            shutil.move(entry, text)
+            print(f"Moved {entry} -> {text}")
+
         elif ext in (".png", ".jpg", ".jpeg"):
             images_count += 1
-        elif ext in (".txt", ".md"):
-            text_count += 1
+            images = Path("D:/Downloads/Images")
+            images.mkdir(parents=True, exist_ok=True)
+            shutil.move(entry, images)
+            print(f"Moved {entry} -> {images}")
+
         else:
             other_types += 1
 
 print(
     f"Total files: {len(entries)}\n"
     f"Subfolders: {folder_count}\n"
-    f"Pdfs: {pdf_count}\n"
     f"Office files: {office_files}\n"
     f"Text files: {text_count}\n"
-    f"CSV files: {csv_count}\n"
     f"Images: {images_count}\n"
     f"Other files: {other_types}"
 )
